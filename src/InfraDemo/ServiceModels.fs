e8b6bc8ea76480d7dbfb8b17abb3fb86aebd31c4
Martin Jul <martin@mjul.com>
2020-11-28 19:40:37 +0000

    Add skeleton project

---
+++ src/InfraDemo/ServiceModels.fs
@@ -0,0 +1,42 @@
+namespace InfraDemo.ServiceModels
+
+type Entity =
+| Service of name:string
+
+/// A model of the application on the service-level
+type ServiceModel = | ServiceModel of Entity list
+module ServiceModel =
+    let createModel es =
+        es |> ServiceModel         
+
+    module Rules = 
+        type ValidationResult =
+        | Valid of ruleName:string
+        | Invalid of ruleName:string * errors:ValidationError list
+        and ValidationError = 
+        | NonUniqueName of name:string
+
+        let validateNamesMustBeUnique (ServiceModel entities) = 
+            let ruleName = "Names must be unique"
+            let nonUniques = 
+                entities 
+                |> List.countBy (function | Service name -> name) 
+                |> List.filter (fun (name, count) -> count>1)
+            match List.isEmpty nonUniques with
+            | true -> Valid(ruleName)
+            | false -> Invalid(ruleName, nonUniques |> List.map (fun (name, _) -> NonUniqueName name))
+
+    /// Validate the ServiceModel
+    let validate sm =
+        seq {
+            yield Rules.validateNamesMustBeUnique sm
+        }
+
+    /// Check if the ServiceModel is valid.
+    let isValid sm =
+        sm 
+        |> validate 
+        |> Seq.exists (function 
+                        | Rules.Valid(_) -> false 
+                        | _ -> true) 
+        |> not
---
+++ tests/InfraDemo.Tests/ServiceModelsTests.fs
@@ -0,0 +1,37 @@
+namespace InfraDemo.Tests
+
+open System
+open Xunit
+
+module ServiceModelsTests = 
+    open InfraDemo.ServiceModels
+    open InfraDemo.ServiceModels.ServiceModel.Rules
+
+    module ServiceModelValidationTests = 
+        [<Fact>]
+        let ``Validate unique names (duplicates case)`` () =
+            let sm = ServiceModel.createModel [Service("foo"); Service("bar"); Service("foo")]
+            let actual = ServiceModel.validate sm 
+            let errors = 
+                actual 
+                |> Seq.collect (function 
+                                | Valid _ -> [] 
+                                | Invalid(_, errs) -> errs) 
+                |> List.ofSeq
+            Assert.False(Seq.isEmpty actual)
+            Assert.True(match errors with
+                        | [NonUniqueName("foo")] -> true
+                        | _ -> false)
+            Assert.False(ServiceModel.isValid sm)
+
+        [<Fact>]
+        let ``Validate unique names (all unique case)`` () =
+            let sm = ServiceModel.createModel [Service("foo"); Service("bar"); Service("baz")]
+            let actual = ServiceModel.validate sm 
+            let invalids = 
+                actual 
+                |> Seq.filter (function 
+                                | Valid _ -> false 
+                                | _ -> true)
+            Assert.True(Seq.isEmpty invalids)
+            Assert.True(ServiceModel.isValid sm)
